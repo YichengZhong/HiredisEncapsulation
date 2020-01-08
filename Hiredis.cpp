fd17b4c315ee6d7da9521f9d3108c3d908e62005
YichengZhong <1165240284@qq.com>
2020-01-08 20:37:19 +0800

    补充函数

--- Hiredis.cpp
+++ Hiredis.cpp
@@ -21,7 +21,8 @@ public:
     int setString(string &key, string &value);
     string getString(string &key);
 
-    int setString(string &key, vector<string> &v_value);
+    int setList(string key, vector<int> value);
+    vector<int> getList(string key);
 
 private:
     //初始化
@@ -204,6 +205,91 @@ int  RedisContext::setString(string &key, string &value)
     freeReplyObject(reply);//释放响应信息
 
     return result;
+}
+
+//向数据库写入vector（list）类型数据
+int RedisTool::setList(string key, vector<int> value)
+{
+    if (m_redis == NULL || m_redis->err)
+    {
+        cout << "Redis init Error !!!" << endl;
+        init();
+        return -1;
+    }
+
+    redisReply *reply;
+
+    int valueSize = value.size();
+    int result = 0;
+
+    for (int i = 0; i < valueSize; i++)
+    {
+        reply = (redisReply*)redisCommand(m_redis, "RPUSH %s %d", key.c_str(), value.at(i));
+        cout << "set list type = " << reply->type << " ";
+        int old = reply->integer;
+        if (reply == NULL)
+        {
+            redisFree(m_redis);
+            m_redis = NULL;
+            result = -1;
+            cout << "set list fail : reply->str = NULL " << endl;
+            //pthread_spin_unlock(&m_redis_flock);
+            return -1;
+        }
+        else if (reply->integer == old++)
+        {
+            result = 1;
+            cout << "rpush list ok" << endl;
+            continue;
+        }
+        else
+        {
+            result = -1;
+            cout << "set list fail ,reply->integer = " << reply->integer << endl;
+            return -1;
+        }
+
+    }
+
+    freeReplyObject(reply);
+    cout << "set List  success" << endl;
+    return result;
+
+}
+
+//从数据库读出vector（list）类型数据
+vector<int> RedisTool::getList(string key)
+{
+
+    if (m_redis == NULL || m_redis->err)
+    {
+        cout << "Redis init Error !!!" << endl;
+        init();
+        return vector<int>{};//返回空的向量
+    }
+
+    redisReply *reply;
+    reply = (redisReply*)redisCommand(m_redis, "LLEN %s", key.c_str());
+    int valueSize = reply->integer;
+    cout << "List size is :" << reply->integer << endl;
+
+    reply = (redisReply*)redisCommand(m_redis, "LRANGE %s %d %d", key.c_str(), 0, valueSize - 1);
+    cout << "get list type = " << reply->type << endl;
+    cout << "get list size = " << reply->elements << endl;//对于数组类型可以用elements元素获取数组长度
+
+    redisReply** replyVector = reply->element;//获取数组指针
+    vector<int> result;
+    for (int i = 0; i < valueSize; i++)
+    {
+        string temp = (*replyVector)->str;//遍历redisReply*数组,存入vector向量
+        int a = atoi(temp.c_str());
+        result.push_back(a);
+        replyVector++;
+    }
+
+    cout << "result size:" << result.size() << endl;
+    return result;
+
 }
 void doTest()
 {
